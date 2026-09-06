5c18d4530f2cf1f9d28b1009f06af07842a343c2
binhtester312 <binhtester312@users.noreply.github.com>
2026-09-06 15:05:32 +0700

    feat: add Approach 2 using Response and TestNG Assert in ParsingJSONResponseData

--- src/test/java/day4/ParsingJSONResponseData.java
+++ src/test/java/day4/ParsingJSONResponseData.java
@@ -1,28 +1,43 @@
 package day4;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class ParsingJSONResponseData {
 
-    @Test(priority = 1)
-    public void testJsonResponse() {
+        @Test(priority = 1)
+        public void testJsonResponse() {
 
-        // Approach 1
+                // Approach 1
+                /*
+                given()
+                                .contentType(ContentType.JSON)
 
-        given()
-                .contentType(ContentType.JSON)
+                                .when()
+                                .get("http://localhost:3000/store")
 
-        .when()
-                .get("http://localhost:3000/store")
+                                .then()
+                                .statusCode(200)
+                                .header("Content-Type", "application/json; charset=utf-8")
+                                .body("book[3].title", equalTo("The Lord of the Rings"));
+                */
 
-        .then()
-                .statusCode(200)
-                .header("Content-Type", "application/json; charset=utf-8");
+                // Approach 2
 
-    }
+                Response res = given()
+                                .contentType(ContentType.JSON)
+
+                                .when()
+                                .get("http://localhost:3000/store");
+
+                Assert.assertEquals(res.getStatusCode(), 200); // validation 1
+                Assert.assertEquals(res.header("Content-Type"), "application/json; charset=utf-8");
+
+        }
 
 }
