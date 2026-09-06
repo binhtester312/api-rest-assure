9365bed987505cec7c6f907669735482716b990e
binhtester312 <binhtester312@users.noreply.github.com>
2026-09-06 14:17:36 +0700

    feat: loop through all headers and print name-value in HeadersDemo

--- src/test/java/day3/HeadersDemo.java
+++ src/test/java/day3/HeadersDemo.java
@@ -2,6 +2,8 @@ package day3;
 
 import org.testng.annotations.Test;
 import io.restassured.response.Response;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -34,8 +36,15 @@ public class HeadersDemo {
                 .get("https://www.google.com/");
 
         // get single header info
-        String headervalue = res.getHeader("Content-Type");
-        System.out.println("The value of Content-type header is: " + headervalue);
+        // String headervalue = res.getHeader("Content-Type");
+        // System.out.println("The value of Content-type header is: " + headervalue);
+
+        // get all headers info
+        Headers myheaders = res.getHeaders();
+
+        for (Header hd : myheaders) {
+            System.out.println(hd.getName() + "    " + hd.getValue());
+        }
 
     }
 
