d7dea35b1466fa88eb5b8bfa4ca095eb5927ecca
binhtester312 <binhtester312@users.noreply.github.com>
2026-09-06 15:15:42 +0700

    feat: parse JSON using JSONObject and print all book titles

--- src/test/java/day4/ParsingJSONResponseData.java
+++ src/test/java/day4/ParsingJSONResponseData.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
@@ -35,11 +36,21 @@ public class ParsingJSONResponseData {
                                 .when()
                                 .get("http://localhost:3000/store");
 
+                /*
                 Assert.assertEquals(res.getStatusCode(), 200); // validation 1
                 Assert.assertEquals(res.header("Content-Type"), "application/json; charset=utf-8");
 
                 String bookname = res.jsonPath().get("book[3].title").toString();
                 Assert.assertEquals(bookname, "The Lord of the Rings");
+                */
+
+                // JSONObject class
+                JSONObject jo = new JSONObject(res.asString()); // converting response to json object type
+
+                for (int i = 0; i < jo.getJSONArray("book").length(); i++) {
+                        String bookTitle = jo.getJSONArray("book").getJSONObject(i).get("title").toString();
+                        System.out.println(bookTitle);
+                }
 
         }
 
