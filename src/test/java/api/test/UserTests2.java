9c8e6110596757e5c131f9605e870656fe4f5516
binhtester312 <binhtester312@users.noreply.github.com>
2026-09-07 16:49:39 +0700

    feat: implement routes.properties and UserEndPoints2 using ResourceBundle

---
+++ src/test/java/api/test/UserTests2.java
@@ -0,0 +1,84 @@
+package api.test;
+
+import api.endpoints.UserEndPoints2;
+import api.payload.User;
+import io.restassured.response.Response;
+import net.datafaker.Faker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class UserTests2 {
+
+    Faker faker;
+    User userPayload;
+    public Logger logger;
+
+    @BeforeClass
+    public void setupData() {
+        faker = new Faker();
+        userPayload = new User();
+
+        userPayload.setId(faker.idNumber().hashCode());
+        userPayload.setUsername(faker.internet().username());
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+        userPayload.setPassword(faker.internet().password(5, 10));
+        userPayload.setPhone(faker.phoneNumber().cellPhone());
+
+        // Obtain logger
+        logger = LogManager.getLogger(this.getClass());
+    }
+
+    @Test(priority = 1)
+    public void testPostUser() {
+        logger.info("********** Creating user (using properties file) **********");
+        Response response = UserEndPoints2.createUser(userPayload);
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("********** User is created **********");
+    }
+
+    @Test(priority = 2)
+    public void testGetUserByName() {
+        logger.info("********** Reading User Info (using properties file) **********");
+        Response response = UserEndPoints2.readUser(this.userPayload.getUsername());
+        response.then().log().all();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("********** User info is displayed **********");
+    }
+
+    @Test(priority = 3)
+    public void testUpdateUserByName() {
+        logger.info("********** Updating User (using properties file) **********");
+        // update data using payload
+        userPayload.setFirstName(faker.name().firstName());
+        userPayload.setLastName(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+
+        Response response = UserEndPoints2.updateUser(this.userPayload.getUsername(), userPayload);
+        response.then().log().body();
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        // Checking data after update
+        Response responseAfterupdate = UserEndPoints2.readUser(this.userPayload.getUsername());
+        Assert.assertEquals(responseAfterupdate.getStatusCode(), 200);
+        logger.info("********** User updated **********");
+    }
+
+    @Test(priority = 4)
+    public void testDeleteUserByName() {
+        logger.info("********** Deleting User (using properties file) **********");
+        Response response = UserEndPoints2.deleteUser(this.userPayload.getUsername());
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+        logger.info("********** User deleted **********");
+    }
+
+}
